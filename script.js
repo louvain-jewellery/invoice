3ee877c056413edc566d912c04eb630958bdd75e
kemanisann <louvainwebsite@gmail.com>
2025-05-31 17:51:06 +0700

    update

--- script.js
+++ script.js
@@ -19,50 +19,52 @@ document.addEventListener("DOMContentLoaded", async () => {
         return;
     }
 
-  const formData = new FormData();
-
-  formData.append("entry.403788479", document.getElementById('customerName').value);
-  formData.append("entry.372677485", document.getElementById('customerPhone').value);
-  formData.append("entry.533089204", document.getElementById('customerEmail').value);
-  formData.append("entry.668745681", document.getElementById('customerAddress').value);
-  formData.append("entry.2022626583", document.getElementById('ringType').value);
-  formData.append("entry.817393732", document.getElementById('sales').value);
-  formData.append("entry.1928733728", document.getElementById('salesPhone').value);
-  formData.append("entry.1798966034", document.getElementById('estimatedCompletion').value);
-
-  formData.append("entry.1691521261", document.getElementById('womenRingMaterial').value);
-  formData.append("entry.787146032", document.getElementById('womenRingGem').value);
-  formData.append("entry.465737398", document.getElementById('womenRingWeight').value);
-  formData.append("entry.255825511", document.getElementById('womenRingSize').value);
-  formData.append("entry.1561675587", document.getElementById('womenRingColor').value);
-  formData.append("entry.258612834", document.getElementById('womenRingFinish').value);
-  formData.append("entry.1519526121", document.getElementById('womenRingEngravement').value);
-  formData.append("entry.684344561", document.getElementById('womenRingModel').value);
-
-  formData.append("entry.1993171059", document.getElementById('menRingMaterial').value);
-  formData.append("entry.1090673640", document.getElementById('menRingGem').value);
-  formData.append("entry.1365428160", document.getElementById('menRingWeight').value);
-  formData.append("entry.1057159300", document.getElementById('menRingSize').value);
-  formData.append("entry.389487441", document.getElementById('menRingColor').value);
-  formData.append("entry.299224605", document.getElementById('menRingFinish').value);
-  formData.append("entry.1452586399", document.getElementById('menRingEngravement').value);
-  formData.append("entry.281323647", document.getElementById('menRingModel').value);
   
-  formData.append("entry.262501710", document.getElementById('notesBox').value);
-  formData.append("entry.443879785", document.getElementById('estimatedPrice').value);
-  formData.append("entry.1527336053", document.getElementById('downPayment').value);
-  formData.append("entry.153171518", document.getElementById('purchasedGold').value);
 
-  fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSd3BbkTWkmq9BN4JsJPKdGgm_9feJElIz9iUJKFrkqjOYuHmQ/formResponse", {
-    method: "POST",
-    mode: "no-cors", // this is essential
-    body: formData
-  }).then(() => {
-    alert("Invoice berhasil dibuat!");
-  }).catch(() => {
-    alert("Invoice gagal dibuat :(");
+    const formData = new FormData();
+
+    formData.append("entry.403788479", document.getElementById('customerName').value);
+    formData.append("entry.372677485", document.getElementById('customerPhone').value);
+    formData.append("entry.533089204", document.getElementById('customerEmail').value);
+    formData.append("entry.668745681", document.getElementById('customerAddress').value);
+    formData.append("entry.2022626583", document.getElementById('ringType').value);
+    formData.append("entry.817393732", document.getElementById('sales').value);
+    formData.append("entry.1928733728", document.getElementById('salesPhone').value);
+    formData.append("entry.1798966034", document.getElementById('estimatedCompletion').value);
+
+    formData.append("entry.1691521261", document.getElementById('womenRingMaterial').value);
+    formData.append("entry.787146032", document.getElementById('womenRingGem').value);
+    formData.append("entry.465737398", document.getElementById('womenRingWeight').value);
+    formData.append("entry.255825511", document.getElementById('womenRingSize').value);
+    formData.append("entry.1561675587", document.getElementById('womenRingColor').value);
+    formData.append("entry.258612834", document.getElementById('womenRingFinish').value);
+    formData.append("entry.1519526121", document.getElementById('womenRingEngravement').value);
+    formData.append("entry.684344561", document.getElementById('womenRingModel').value);
+
+    formData.append("entry.1993171059", document.getElementById('menRingMaterial').value);
+    formData.append("entry.1090673640", document.getElementById('menRingGem').value);
+    formData.append("entry.1365428160", document.getElementById('menRingWeight').value);
+    formData.append("entry.1057159300", document.getElementById('menRingSize').value);
+    formData.append("entry.389487441", document.getElementById('menRingColor').value);
+    formData.append("entry.299224605", document.getElementById('menRingFinish').value);
+    formData.append("entry.1452586399", document.getElementById('menRingEngravement').value);
+    formData.append("entry.281323647", document.getElementById('menRingModel').value);
+    
+    formData.append("entry.262501710", document.getElementById('notesBox').value);
+    formData.append("entry.443879785", document.getElementById('estimatedPrice').value);
+    formData.append("entry.1527336053", document.getElementById('downPayment').value);
+    formData.append("entry.153171518", document.getElementById('purchasedGold').value);
+
+    fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSd3BbkTWkmq9BN4JsJPKdGgm_9feJElIz9iUJKFrkqjOYuHmQ/formResponse", {
+      method: "POST",
+      mode: "no-cors", // this is essential
+      body: formData
+    }).then(() => {
+      alert("Invoice berhasil dibuat!");
+    }).catch(() => {
+      alert("Invoice gagal dibuat :(");
+    });
   });
-});
 
 document.querySelectorAll('.currency').forEach(input => {
   input.addEventListener('input', function (e) {
